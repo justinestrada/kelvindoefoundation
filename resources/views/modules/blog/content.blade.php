4cba4aa9a5389da204bf51ba8e36c25c61491bfd
Justin Estrada <jebusiness723@gmail.com>
2021-10-17 23:48:10 -0700

    Blog hero

--- resources/views/modules/blog/content.blade.php
+++ resources/views/modules/blog/content.blade.php
@@ -1,19 +1,19 @@
 
 <div class="container">
-  @php
-  $categories = get_categories([
-    'orderby' => 'name',
-    'order' => 'ASC'
-  ]);
-  @endphp
-  @if ($categories)
-    <div class="d-flex flex-row py-3 mb-3 blog-categories-row">
-      <a href="/blog" class="px-3 py-2 text-black">All</a>
+  <div class="blog-categories-row d-flex flex-row py-3 mb-3">
+    <a href="/blog" class="text-black px-3 py-2 active">All</a>
+    @php
+    $categories = get_categories([
+      'orderby' => 'name',
+      'order' => 'ASC'
+    ]);
+    @endphp
+    @if ($categories)
       @foreach ($categories as $category)
-        <a href="{{ esc_url( get_category_link( $category->term_id ) ) }}" class="px-3 py-2 text-black">{{ $category->name }}</a>
+        <a href="{{ esc_url( get_category_link( $category->term_id ) ) }}" class="text-black px-3 py-2">{{ $category->name }}</a>
       @endforeach
-    </div>
-  @endif
+    @endif
+  </div>
   <div class="row">
     <div class="col-sm-3">
       <div class="blog-sidebar">
--- resources/views/modules/blog/hero.blade.php
+++ resources/views/modules/blog/hero.blade.php
@@ -1,12 +1,15 @@
 
-@if ($hero = get_field('hero'))
+@php
+$blog_page_id = (int)get_option( 'page_for_posts' );    
+@endphp
+@if ($hero = get_field('hero', $blog_page_id))
   <div class="blog-hero-wrapper" style="background-image: url('{{ $hero['image'] }}')">
     <div class="blog-hero-wrapper-container d-flex align-items-center">
       <div class="container blog-hero-wrapper-content">
         @if (function_exists('aioseo_breadcrumbs'))
           {!! do_shortcode('[aioseo_breadcrumbs]') !!}
         @endif
-        <h1>{!! get_the_title() !!}</h1>
+        <h1>{!! get_the_title($blog_page_id) !!}</h1>
         <div>
           {!! $hero['content'] !!}
         </div>
